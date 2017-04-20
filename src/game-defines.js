c38bbff310af2ee013fd2fd716be65720b03f448
楚浩远 <328387458@qq.com>
2017-04-20 13:24:45 +0800

    game1

---
+++ src/game-defines.js
@@ -0,0 +1,6 @@
+/**
+ * Created by chuhaoyuan on 2017/4/20.
+ */
+var defines = {};
+defines.resPath = './asset/';
+export default defines;
---
+++ src/resources.js
@@ -0,0 +1,12 @@
+import defines from'./game-defines.js'
+const res =
+{
+  "background_2": "background_2.png", 
+  "background_3": "background_3.png", 
+  "bj1": "bj1.png", 
+  "direction_2": "direction_2.png", 
+  "dragon_8": "dragon_8.png", 
+  "dragon_head": "dragon_head.png"
+}
+ for (let i in res) {res[i] = defines.resPath+res[i];}
+ export default res
